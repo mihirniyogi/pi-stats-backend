2e95dc02f9317af45d1d450fab96c55a7d2ee420
mihirniyogi <e1122052@u.nus.edu>
2024-11-08 12:04:53 +0800

    added cors headers

--- project/settings.py
+++ project/settings.py
@@ -43,17 +43,32 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS Configuration
+
+# Origins allowed to request
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:5173",
+  "https://rpi.mihirniyogi.com/"
+]
+
+# Only GET method allowed
+CORS_ALLOW_METHODS = [
+  "GET"
 ]
 
 ROOT_URLCONF = 'project.urls'
